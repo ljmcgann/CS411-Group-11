ed6df009b27bea833226a60dfae46ed5aac3540d
ecyecyecyecy <ecyecyecyecy@users.noreply.github.com>
2018-04-29 16:03:53 -0400

    Update visionex.py
    the version makes the output's type <str>, which allows us to store it in muSQL.

--- visionex.py
+++ visionex.py
@@ -2,39 +2,60 @@
 
 Then must write in command line: 'export GOOGLE_APPLICATION_CREDENTIALS=apikey.json'. 
 
-Then to run you have to download a picture into the folder and then add the picture name as an argument...
-python visionex.py pictureName          
-
 This will output the safe_search api which is basically if the picture is violent, for adults, about injuries(medical)
-and inappropriate context overall. 
+and inappropriate context overall.
 
-The code i have commented out is to use the labels api which just describes what 
-is shown in the picture with basic labels (nothing to do with violence but very descriptive). 
 
+in terminal, as example, write following:
+
+python visionex.py safe-search ./rick.jpg
+
+notice that   ./rick.jpg    is the path of picture you want to use
 """
 
-
-
+import argparse
 import io
 import os
 from google.cloud import vision
 from google.cloud.vision import types
 import sys
-images = [sys.argv[1]]
-#print sys.argv[1]
-vision_client = vision.ImageAnnotatorClient()
-#images = ['war_tank.jpg', 'isis.jpg', 'bleedingman.jpg']
-#images = ['Selena_Gomez_-_Walmart_Soundcheck_Concert.jpg', 'selena2.jpg', 'selena3.jpg', 'selena4.jpg', 'selena5.jpg', 'selenagomez.jpg']
 
-for x in images:
-    with io.open(x, 'rb') as image:
-        content = image.read()
-        image = types.Image(content=content)
 
-	#response = vision_client.label_detection(image=image)
-    #labels = response.label_annotations
-    response = vision_client.safe_search_detection(image=image)
-    labels = response.safe_search_annotation
-    print labels
-    #for label in labels:
-    #print label.description #, label.score
+def detect_safe_search(path):
+    """Detects unsafe features in the file."""
+    client = vision.ImageAnnotatorClient()
+    
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+    
+    image = vision.types.Image(content=content)
+
+    response = client.safe_search_detection(image=image)
+    safe = response.safe_search_annotation
+    
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+    print('Safe search:')
+    print(type('adult: {}'.format(likelihood_name[safe.adult])))
+    print('medical: {}'.format(likelihood_name[safe.medical]))
+    print('spoofed: {}'.format(likelihood_name[safe.spoof]))
+    print('violence: {}'.format(likelihood_name[safe.violence]))
+
+
+def run_local(args):
+    if args.command == 'safe-search':
+        detect_safe_search(args.path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                                     description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    subparsers = parser.add_subparsers(dest='command')
+
+    safe_search_parser = subparsers.add_parser(
+                                               'safe-search', help=detect_safe_search.__doc__)
+    safe_search_parser.add_argument('path')
+
+    args = parser.parse_args()
+    run_local(args)
